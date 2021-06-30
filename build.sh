798e6d48e741d0bc08d899c357bb7627557dcfa6
Ramsay Domloge <rdomloge@gmail.com>
2021-06-30 16:25:55 +0100

    Hopefully ironed out all the bugs and turned it into a useable script

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ echo export SSHPASS
 
 
 
-pubkey=$(</home/domloger/.ssh/id_rsa.pub)
+pubkey='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDG4kPSGvLp9hPue4UolYIW9Rf05rnkcSrcRTp7SzV1Ybs/NTyRirk07QB/R8n3de4rEc0FvXsvtjRQzGkACqr5sYywqpghx9NzhNidgZlaDHoBOHFR6aDwiQKBLHtD8z0eHyL7fNYE3qx3+uxh0zMuKzmTitYSVgGVju8bl5zef5R4JQ5+P8kJQggvPWlWUTdo1gJ+yGO7Bn+o5GdsR+v3twc3SVrNgDoKIGXlTSk9E6J4dKASCYkaFme7LSxrw3c9sify8sjc2CFqMvCse6NJT+uZkzSMBixn6uK8mSULJlRk6OF7eki8xK4Iqfwn2T8Ywg81FqdEGxAWjdtu3CjRlR5gH1Cx9hijoZJpWX+r2YGij1Frqe06O+YC3UNIH9SFyeNlbsE2lmhyH+1J3YxJn6jhFgX5sjxz+YKUMS/9+kXazjVK7oBVJTxwvLD+0oriUJ3bAeX1KGJ9+Oe9sHRuc3LieSIXMEggXRDTvZLad5tJybeyEcB/XHUKXC4bF8VroYN3oOep/sAx7c5B+BQvYeX+MJlb9p18+f7YULhG6DwuhZuhnlSqimUs81eeuGuzgSm6/WaJ3H7d5ctc20So+OfPovXD80N7c0/T+WMYEIK/bwljgsSskvnpZpQXEnqXXyFShBw5l61TFqo5e7FQeA1A94pXQbFAuAg/MFZCtw== bca\domloger@LP032542'
 echo "$pubkey"
 
 master=10.0.0.12
@@ -22,42 +22,55 @@ for i in "${all[@]}"
 do
    :
    echo Adding key to $i
-   sshpass -e ssh ubuntu@$i "mkdir -p .ssh && touch .ssh/authorized_keys && echo $pubkey >> .ssh/authorized_keys"
+   sshpass -e ssh -oStrictHostKeyChecking=no ubuntu@$i "mkdir -p .ssh && touch .ssh/authorized_keys && echo $pubkey >> .ssh/authorized_keys"
    echo Key copied
-   sshpass -e ssh ubuntu@$i "sudo bash -c 'echo $cmdlinecontents > $cmdlinefile'"
+   sshpass -e ssh -oStrictHostKeyChecking=no ubuntu@$i "sudo bash -c 'echo $cmdlinecontents > $cmdlinefile'"
    echo Enabled container features - REBOOTING $i
-   sshpass -e ssh ubuntu@$i sudo reboot
+   sshpass -e ssh -oStrictHostKeyChecking=no ubuntu@$i sudo reboot
 done
 
 echo Waiting for nodes to reboot
 sleep 60
 
+
+version=v1.21.2+k3s1
+echo Version of k3s is $version
+
 echo Generating key pair on master
-ssh ubuntu@$master ssh-keygen -q -f /home/ubuntu/.ssh/id_rsa -P \"\"
+sshpass -e ssh ubuntu@$master ssh-keygen -q -f /home/ubuntu/.ssh/id_rsa -P \"\"
 
 echo Adding keypair to its own authorised keys
-ssh ubuntu@$master "cat .ssh/id_rsa.pub >> .ssh/authorized_keys"
+sshpass -e ssh ubuntu@$master "cat .ssh/id_rsa.pub >> .ssh/authorized_keys"
 
 echo Installing k3sup on master
 ssh ubuntu@$master "curl -sLS https://get.k3sup.dev | sh"
 ssh ubuntu@$master sudo cp k3sup-arm64 /usr/local/bin/k3sup 
 
 echo Setting up k3s on master
-ssh ubuntu@$master k3sup install --ip $master --user ubuntu
+ssh ubuntu@$master k3sup install --ip $master --user ubuntu --k3s-version=$version
 
 echo Capturing master public key to copy to slaves
 masterkey=$(ssh ubuntu@$master cat .ssh/id_rsa.pub)
 echo Master key: $masterkey
 
+# Before we join the nodes, we have to rename all of them otherwise k3s won't allow them to join with existing names
+ssh ubuntu@$master "sudo bash -c 'echo k8s-master-1 > /etc/hostname' && sudo reboot"
+COUNTER=1
+for i in "${nodes[@]}"
+do
+   :
+   echo Renaming node $i
+   ssh ubuntu@$i "sudo bash -c 'echo k8s-node-$COUNTER > /etc/hostname' && sudo reboot"
+   let COUNTER++
+done
 
 for i in "${nodes[@]}"
 do
    :
    echo Adding master public key to node $i
-   ssh ubuntu@$i "echo $masterkey >> .ssh/authorized_keys" 
+   sshpass -e ssh ubuntu@$i "echo $masterkey >> .ssh/authorized_keys"
    echo Creating cluster node on $i
-   ssh ubuntu@$master k3sup join --ip $i --server-ip $master --user ubuntu
-   ssh ubuntu
+   ssh ubuntu@$master k3sup join --ip $i --server-ip $master --user ubuntu --k3s-version=$version
 done
 
 for i in "${all[@]}"
@@ -78,3 +91,5 @@ echo Installing OpenFaaS CLI
 ssh ubuntu@$master "curl -sSL https://cli.openfaas.com | sh"
 ssh ubuntu@$master sudo cp faas-cli-arm64 /usr/local/bin/faas-cli
 ssh ubuntu@$master sudo ln -sf /usr/local/bin/faas-cli /usr/local/bin/faas
+
+rem https://github.com/carlosedp/cluster-monitoring.git
