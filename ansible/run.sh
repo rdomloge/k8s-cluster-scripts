c6a953c74a4690e99b826e6a3986165f27b1558d
Ramsay Domloge <domloger@AD.BCA.COM>
2022-07-28 10:35:18 +0100

    Initial working draft

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,5 +1,7 @@
-HOSTS_MOUNT="--mount type=bind,source="${PWD}"/hosts,target=/etc/ansible/hosts"
-PB_MOUNT="--mount type=bind,source="${PWD}"/testrunbook.yaml,target=/testrunbook.yaml"
+HOSTS_MNT="--mount type=bind,source="${PWD}"/hosts,target=/etc/ansible/hosts"
+PB_MNT="--mount type=bind,source="${PWD}"/testrunbook.yaml,target=/testrunbook.yaml"
+KNOWN_USERS_MNT="--mount type=bind,source="${PWD}"/known_hosts,target=/root/.ssh/known_hosts"
+CFG_MNT="--mount type=bind,source="${PWD}"/ansible.cfg,target=/etc/ansible/ansible.cfg"
+VARS_MNT="--mount type=bind,source="${PWD}"/vars.yml,target=/vars.yml"
 
-docker run $HOSTS_MOUNT $PB_MOUNT -ti rdomloge/ansible ansible-playbook testrunbook.yaml
-# ansible all -m ping -u ubuntu
+docker run $HOSTS_MNT $PB_MNT  $CFG_MNT $VARS_MNT -ti rdomloge/ansible ansible-playbook -v testrunbook.yaml
