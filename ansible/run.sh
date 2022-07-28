0fdd232168fe4f64cf29c680ca37e1ba69ab6914
Ramsay Domloge <domloger@AD.BCA.COM>
2022-07-28 10:36:27 +0100

    Initial working draft

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,6 +1,5 @@
 HOSTS_MNT="--mount type=bind,source="${PWD}"/hosts,target=/etc/ansible/hosts"
 PB_MNT="--mount type=bind,source="${PWD}"/testrunbook.yaml,target=/testrunbook.yaml"
-KNOWN_USERS_MNT="--mount type=bind,source="${PWD}"/known_hosts,target=/root/.ssh/known_hosts"
 CFG_MNT="--mount type=bind,source="${PWD}"/ansible.cfg,target=/etc/ansible/ansible.cfg"
 VARS_MNT="--mount type=bind,source="${PWD}"/vars.yml,target=/vars.yml"
 
