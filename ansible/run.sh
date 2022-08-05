e0893e8f29731bdf7658a8d480a970740c7e77bd
Ramsay Domloge <domloger@AD.BCA.COM>
2022-08-05 19:01:34 +0100

    Trying to break up the files and include them in a parent

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,12 +1,13 @@
 HOSTS_MNT="--mount type=bind,source="${PWD}"/hosts,target=/etc/ansible/hosts"
-PREP_PB_MNT="--mount type=bind,source="${PWD}"/prep.yaml,target=/prep.yaml"
-SWAP_PB_MNT="--mount type=bind,source="${PWD}"/enable_swap.yaml,target=/enable_swap.yaml"
-MASTER_PB_MNT="--mount type=bind,source="${PWD}"/master.yaml,target=/master.yaml"
-NODES_PB_MNT="--mount type=bind,source="${PWD}"/nodes.yaml,target=/nodes.yaml"
+YAML_PB_MNT="--mount type=bind,source="${PWD}/runbooks",target=/yaml"
 CFG_MNT="--mount type=bind,source="${PWD}"/ansible.cfg,target=/etc/ansible/ansible.cfg"
 VARS_MNT="--mount type=bind,source="${PWD}"/vars.yml,target=/vars.yml"
 
-docker run $HOSTS_MNT $PREP_PB_MNT  $CFG_MNT $VARS_MNT -ti rdomloge/ansible ansible-playbook -v prep.yaml
-#docker run $HOSTS_MNT $SWAP_PB_MNT  $CFG_MNT $VARS_MNT -ti rdomloge/ansible ansible-playbook -v enable_swap.yaml
-docker run $HOSTS_MNT $MASTER_PB_MNT  $CFG_MNT $VARS_MNT -ti rdomloge/ansible ansible-playbook -v master.yaml
-docker run $HOSTS_MNT $NODES_PB_MNT  $CFG_MNT $VARS_MNT -ti rdomloge/ansible ansible-playbook -v nodes.yaml
+#docker run $HOSTS_MNT $YAML_PB_MNT  $CFG_MNT $VARS_MNT -ti rdomloge/ansible ansible-playbook -v yaml/login.yaml
+#docker run $HOSTS_MNT $YAML_PB_MNT  $CFG_MNT $VARS_MNT -ti rdomloge/ansible ansible-playbook -v yaml/virtualbox-login.yaml
+#docker run $HOSTS_MNT $YAML_PB_MNT  $CFG_MNT $VARS_MNT -ti rdomloge/ansible ansible-playbook -v yaml/prep.yaml
+#docker run $HOSTS_MNT $YAML_PB_MNT  $CFG_MNT $VARS_MNT -ti rdomloge/ansible ansible-playbook -v yaml/enable_swap.yaml
+#docker run $HOSTS_MNT $YAML_PB_MNT  $CFG_MNT $VARS_MNT -ti rdomloge/ansible ansible-playbook -v yaml/master.yaml
+#docker run $HOSTS_MNT $YAML_PB_MNT  $CFG_MNT $VARS_MNT -ti rdomloge/ansible ansible-playbook -v yaml/nodes.yaml
+
+docker run $HOSTS_MNT $YAML_PB_MNT  $CFG_MNT -ti rdomloge/ansible ansible-playbook -v yaml/k8s-runbook.yaml
