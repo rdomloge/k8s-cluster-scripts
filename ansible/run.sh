24402815588a754e2caa3dafcac135a1d1aa1696
Ramsay Domloge <domloger@AD.BCA.COM>
2022-08-08 22:58:37 +0100

    First version of the files that work all the way through for 1xvirtualbox master, 2 rpi nodes, from scratch

--- ansible/run.sh
+++ ansible/run.sh
@@ -3,11 +3,4 @@ YAML_PB_MNT="--mount type=bind,source="${PWD}/runbooks",target=/yaml"
 CFG_MNT="--mount type=bind,source="${PWD}"/ansible.cfg,target=/etc/ansible/ansible.cfg"
 VARS_MNT="--mount type=bind,source="${PWD}"/vars.yml,target=/vars.yml"
 
-#docker run $HOSTS_MNT $YAML_PB_MNT  $CFG_MNT $VARS_MNT -ti rdomloge/ansible ansible-playbook -v yaml/login.yaml
-#docker run $HOSTS_MNT $YAML_PB_MNT  $CFG_MNT $VARS_MNT -ti rdomloge/ansible ansible-playbook -v yaml/virtualbox-login.yaml
-#docker run $HOSTS_MNT $YAML_PB_MNT  $CFG_MNT $VARS_MNT -ti rdomloge/ansible ansible-playbook -v yaml/prep.yaml
-#docker run $HOSTS_MNT $YAML_PB_MNT  $CFG_MNT $VARS_MNT -ti rdomloge/ansible ansible-playbook -v yaml/enable_swap.yaml
-#docker run $HOSTS_MNT $YAML_PB_MNT  $CFG_MNT $VARS_MNT -ti rdomloge/ansible ansible-playbook -v yaml/master.yaml
-#docker run $HOSTS_MNT $YAML_PB_MNT  $CFG_MNT $VARS_MNT -ti rdomloge/ansible ansible-playbook -v yaml/nodes.yaml
-
 docker run $HOSTS_MNT $YAML_PB_MNT  $CFG_MNT -ti rdomloge/ansible ansible-playbook -v yaml/k8s-runbook.yaml
