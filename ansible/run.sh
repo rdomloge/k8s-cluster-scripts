3ce6a7ff5c5ede627a6ecad5233558c7f4db655c
Ramsay Domloge <domloger@AD.BCA.COM>
2022-08-10 01:52:28 +0100

    Got Ubuntu 22 working with a fix for the vxlan install and it's weird post install UI and some async bits that make the scripts a bit quicker

--- ansible/run.sh
+++ ansible/run.sh
@@ -3,4 +3,6 @@ YAML_PB_MNT="--mount type=bind,source="${PWD}/runbooks",target=/yaml"
 CFG_MNT="--mount type=bind,source="${PWD}"/ansible.cfg,target=/etc/ansible/ansible.cfg"
 VARS_MNT="--mount type=bind,source="${PWD}"/vars.yml,target=/vars.yml"
 
-docker run $HOSTS_MNT $YAML_PB_MNT  $CFG_MNT -ti rdomloge/ansible ansible-playbook -v yaml/k8s-runbook.yaml
+docker run $HOSTS_MNT $YAML_PB_MNT  $CFG_MNT -ti rdomloge/ansible ansible-playbook yaml/k8s-runbook.yaml
+
+#docker run $HOSTS_MNT $YAML_PB_MNT  $CFG_MNT -ti rdomloge/ansible ansible-playbook -v yaml/resetpasswd.yaml
